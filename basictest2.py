813fa23efd46035d03a3420eb5cc44431558bf8c
isowa <isowa@eonerc.rwth-aachen.de>
2018-02-28 10:29:32 +0100

    to windows migration

--- basictest2.py
+++ basictest2.py
@@ -1,38 +1,19 @@
+import time
 from osbrain import run_agent
 from osbrain import run_nameserver
-from osbrain import Agent
 
 
-def log_message(self, message):
-    self.log_info('Received: %s' % message)
-
-
-def handler2(self, message):
-    self.log_info('Handler2: %s' % message)
-
-
-class Greeter(Agent):
-    def on_init(self):
-        self.bind('PUSH', alias='main')
-
-    def hello(self, name):
-        self.send('main', 'Hello, %s!' % name)
+def log_message(agent, message):
+    agent.log_info('Received: %s' % message)
 
 
 if __name__ == '__main__':
 
     # System deployment
     ns = run_nameserver()
-    alice = run_agent('Alice', base=Greeter)
+    alice = run_agent('Alice')
     bob = run_agent('Bob')
-    run_agent('Agent2')
 
-    # show all agents
-    for alias in ns.agents():
-        print(alias)
-
-    # push pull a-b
-    addr = alice.bind('PUSH', alias='main')
-    bob.connect(addr, handler=[log_message, handler2]) #  This handler will be serialized and stored in the remote agent to be executed there when needed.
+    print(alice.name)
 
     ns.shutdown()
--- vpps_only.py
+++ vpps_only.py
@@ -8,23 +8,37 @@ from osbrain import Agent
 
 import matplotlib.pyplot as plt
 
-data = ["vpp1", "vpp2", "vpp3", "vpp4"]
-vpp_n = len(data)
+data_names = ["vpp1", "vpp2", "vpp3", "vpp4"]
+data_paths = ["data/vpp1.json", "data/vpp2.json", "data/vpp3.json", "data/vpp4.json"]
+vpp_n = len(data_names)
 ts_n = 10
 # connections for negotiations defined (squere)
 adj_matrix = np.array([[True, True, True, False],
-                          [True, True, False, True],
-                          [True, False, True, True],
-                          [False, True, True, True]], dtype=bool)
+                       [True, True, False, True],
+                       [True, False, True, True],
+                       [False, True, True, True]], dtype=bool)
+
+
+class VPP_ext_agent(Agent):
+    def on_init(self):
+        #self.bind('PUSH', alias='main')
+        pass
+
+    def load_data(self, path):
+        with open(path, 'r') as f:
+            arr = json.load(f)
+        return arr
+
+    def status_report(self, name):
+        self.send('main', 'Hello, %s!' % name)
 
 def print_data():
-
     gen_sum = np.zeros(ts_n)
     load_sum = np.zeros(ts_n)
 
     plt.figure(1)
     for vi in range(vpp_n):
-        with open("data/"+data[vi]+".json", 'r') as f:
+        with open("data/" + data_names[vi] + ".json", 'r') as f:
             ar = json.load(f)
 
         plt.subplot(vi + 221)
@@ -34,22 +48,20 @@ def print_data():
         plt.setp(gen, 'color', 'b', 'linewidth', 2.0)
         plt.setp(load, 'color', 'r', 'linewidth', 2.0)
 
-        plt.ylabel('gen/load aggr. power '+data[vi])
+        plt.ylabel('gen/load aggr. power ' + data_names[vi])
 
         gen_sum = np.sum([gen_sum, np.array([ar["generation"]])], axis=0)
         load_sum = np.sum([load_sum, np.array([ar["load"]])], axis=0)
 
-    plt.figure(2) # sum of all laods generations etc.
-    print(gen_sum)
+    plt.figure(2)  # sum of all laods generations etc.
     gen = plt.plot(gen_sum[0])
     load = plt.plot(load_sum[0])
     plt.setp(gen, 'color', 'b', 'linewidth', 2.0)
     plt.setp(load, 'color', 'r', 'linewidth', 2.0)
     plt.ylabel('total gen/load power of the system')
 
-
-def log(self, message):
-    self.log_info('Log a: %s' % message)
+def request_log(self, message):
+    self.log_info('Request log: %s' % message)
 
 
 if __name__ == '__main__':
@@ -59,48 +71,54 @@ if __name__ == '__main__':
     # initialization of the agents
     ns = run_nameserver()
     for vpp_idx in range(vpp_n):
-        run_agent(data[vpp_idx])
+        agent = run_agent(data_names[vpp_idx], base=VPP_ext_agent)
+        agent.set_attr(myname=str(data_names[vpp_idx]))
+        print(agent.myname)
 
     # Show agents registered in the name server
-    for alias in ns.agents():
-        print(alias)
+    # for alias in ns.agents():
+    #    print(alias)
+    # print("----------------------")
 
-    print("----------------------")
-
-    # subscriptions to neighbours
+    # subscriptions to neighbours only
     for vpp_idx in range(vpp_n):
 
-        agent = ns.proxy(data[vpp_idx])
-        addr = agent.bind('PUB', alias='main')
+        agent = ns.proxy(data_names[vpp_idx])
+        addr = agent.bind('PUB', alias='main') # this agent will publish to neighbours
 
         for n in range(vpp_n):
-            if n==vpp_idx:
+            if n == vpp_idx:
                 continue
-            if adj_matrix[vpp_idx][n]==True:
-                neighbour = ns.proxy(data[n])
-                neighbour.connect(addr, handler={'a': log})
+            if adj_matrix[vpp_idx][n] == True:
+                neighbour = ns.proxy(data_names[n])
+                neighbour.connect(addr, handler={'request_topic': request_log}) # only neighbours connect to the agent
 
-    # system time loop (10 timesteps)
+    # system time loop (10 timesteps for now)
     for t in range(ts_n):
 
-        # each agent publishes deficit or exccess to the neighbours
         for vpp_idx in range(vpp_n):
-            agent = ns.proxy(data[vpp_idx])
+            agent = ns.proxy(data_names[vpp_idx])
 
-        # Send messages
-        for i in range(6):
-            time.sleep(1)
-            message = 'Hello, %s!' % topic
-            alice.send('main', message, topic=topic)
+            # each agent checks the internal resources (from data or from internal agent (OPF))
+            json_data = agent.load_data(data_paths[vpp_idx])
 
+            # each deficit agent publishes deficit to the neighbours only
+            if json_data["generation"][t] - json_data["load"][t] > 0:
+                agent.log_info("I am excess")
+            elif json_data["generation"][t] - json_data["load"][t] < 0:
+                agent.log_info("I am deficit. I'll publish requests to neighbours.")
 
+                message_request = 'I am '+ ns.agents()[vpp_idx] +'. I need support!'
+                agent.send('main', message_request, topic='request_topic')
+
+            else:
+                agent.log_info("I am balanced")
+
+            # each asked and interested excess agent publishes the price curve, based on prices from data
+            # each
+
+        time.sleep(1)
 
     ns.shutdown()
 
     plt.show()
-
-
-
-
-
-
