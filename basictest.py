0e8a806f579d47545d06c4b093a5d1ee195bf2c1
igorsowa9 <igorsowa9@gmail.com>
2018-04-10 18:10:09 +0200

    next step:
    integrate power balance calculation based on mpc and new data file into the negotiation file,
    integrate with next step i.e. requests making.

--- basictest.py
+++ basictest.py
@@ -1,12 +1,33 @@
+from osbrain import Agent
+import json
+import settings
+import time
+from pprint import pprint as pp
+from settings import data_names, data_names_dict, data_paths, vpp_n, ts_n, adj_matrix, print_data
+
+import time
+import sys
+from osbrain import run_agent
+from osbrain import run_nameserver
+from pprint import pprint as pp
+import copy
+
+from settings import data_names, data_names_dict, data_paths, vpp_n, ts_n, adj_matrix, print_data, system_status, \
+    small_wait, price_increase_factor
+from other_agents import VPP_ext_agent
+from utilities import system_consensus_check, erase_iteration_memory, erase_timestep_memory
+
+
 from oct2py import octave
 
 octave.addpath('/home/iso/PycharmProjects/vpp/matpow_cases')
 octave.addpath('/home/iso/PycharmProjects/vpp/matpower6.0')
 octave.addpath('/home/iso/PycharmProjects/vpp/matpower6.0/t')
 
-# load basic topology
-# update the current generation constraints (weather), fixed loads (curves loads) or flexible loads constraints
+# - load basic topology
+# - update the current generation constraints (weather), fixed loads (curves loads) or flexible loads constraints
 #   (but usually they are fixed according to contracts with clients)
+# - run first opf for status definition with only own resources -> after this requests distribution
 # update bid offers in own system i.e. simulate what would be if you accept them, i.e. run OPF:
 #   * to be modified:
 #       - load at slack bus as negative generator
@@ -18,10 +39,38 @@ octave.addpath('/home/iso/PycharmProjects/vpp/matpower6.0/t')
 #   - EXTENSION: maximize the revenue within the bid offer
 
 
-ppc = octave.case5_vpp()
+def system_state_update_and_balance(mpc_t, t, data):
+    generation = data['generation']
+    load = data['load']
+    price = data['price']
+    slack_idx = data['slack_idx']
+
+    mpc_t['bus'][:, 2] = load[t]
+    mpc_t['gen'][:, 8] = generation[t]
+    mpc_t['gencost'][:, 4] = price[t]
+
+    res = octave.rundcopf(mpc_t, octave.mpoption('out.all', 1))
+    power_balance = res['gen'][slack_idx, 1]
+
+    return power_balance
+
+
+ns = run_nameserver()
+
+a = run_agent('tester', base=VPP_ext_agent)
+data = a.load_data("data/vpp1-case5.json")
+
+mpc = octave.case5_vpp()
 mpopt = octave.mpoption('out.all', 1)
-r = octave.rundcopf(ppc, mpopt)
+
+gens = system_state_update_and_balance(copy.deepcopy(mpc), 1, data)
+
+print(gens)
+sys.exit()
+
+r = octave.rundcopf(mpc, mpopt)
 print('SUCCESS?: ', r['success'])
 
 
 def upload_bid_to_mpc(mpc0, bids):
+    pass
--- other_agents.py
+++ other_agents.py
@@ -18,19 +18,39 @@ class VPP_ext_agent(Agent):
         pass
 
     def load_data(self, path):
+        """
+        Loads data for VPP from file, from web, whatever necessary.
+        :param path:
+        :return:
+        """
         with open(path, 'r') as f:
             arr = json.load(f)
         return arr
 
     def current_balance(self, time):
+        """
+        This should be internal PF in order to define excess/deficit. I returns the excess/deficit at the PCC.
+        :param time:
+        :return:
+        """
         json_data = self.load_data(data_paths[data_names_dict[self.name]])
         return float(json_data["generation"][time] - json_data["load"][time])
 
     def current_price(self, time):
+        """
+        Loads price of own resources / create price curve, at a time.
+        :param time:
+        :return: price value / price curve
+        """
         json_data = self.load_data(data_paths[data_names_dict[self.name]])
         return json_data["price"][time]
 
     def new_opf(self):
+        """
+        After a deficit agent receives all price curves, it should define bids through running internal opf
+        as defined in this function.
+        :return: bids that are sent back to the excess agents of interest
+        """
         # opf asa system is integrated
         memory = self.get_attr('iteration_memory_pc')
         need = abs(self.get_attr('current_status')[1])
@@ -54,9 +74,21 @@ class VPP_ext_agent(Agent):
         return bids
 
     def set_consensus_if_norequest(self):
+        """
+        This is called in case an excess agent does not have any requests from deficit agents in the deficit loop.
+        :return:
+        """
 
         time.sleep(0.1)
         if self.get_attr('n_requests') == 0:
             self.log_info('I am E and I nobody wants to buy from me (n_requests=' + str(self.get_attr('n_requests')) +
                           '). I set consensus.')
             self.set_attr(consensus=True)
+
+    def deficit_opf(self, price_curves):
+        """
+        After a deficit agent receives all price curves, it should define bids through running internal opf
+        as defined in this function.
+        :param price_curves
+        :return: bids that are sent back to the excess agents of interest
+        """
