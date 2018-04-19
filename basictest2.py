0d1a68a82fd761a2e4ce759222cae0b1065b9191
igorsowa9 <igorsowa9@gmail.com>
2018-04-19 19:51:37 +0200

    next steps:
    finish time 2 scenario with no secondary opf:
    fix the showing deals at the end
    make secondary opf
    make other scenarios
    ML data and implementation, where, what.
    
    DATA in JSON uploaded - plan and import to OPF, pursue towards the pure ML derivation i.e. prediction of adjacent power balances to propose better negotiations? - with no agents, only pure ML on an isolated system case and opfs...

--- basictest2.py
+++ basictest2.py
@@ -2,6 +2,10 @@ from pypower.api import *
 from case5_vpp import case5_vpp
 from rundcopf_noprint import rundcopf
 
+from other_agents import VPP_ext_agent
+
+
+
 ppc = case5_vpp()
 opt = ppoption(VERBOSE=1)
 r = rundcopf(ppc, opt)
